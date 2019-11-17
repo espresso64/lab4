9463d2fad79bd411050eacf12936d037a2e33080
EC2 Default User <ec2-user@ip-172-31-22-234.us-west-2.compute.internal>
2019-11-17 12:03:30 +0000

    4 planets added

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require ("express");
 const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Running Express Server..."); 
 });
+
+
 
 //routes
 app.get("/", function (req,res){
@@ -13,13 +17,25 @@ app.get("/", function (req,res){
 
 app.get("/mercury", function(req,res){
     res.render("mercury.html");
-    
 });
 
 app.get("/venus", function(req,res){
-    res.send ("This will be Venus web page!");
-    
+    res.render("venus.html");
 });
+
+app.get("/earth", function(req,res){
+    res.render("earth.html");
+});
+
+app.get("/mars", function(req,res){
+    res.render("mars.html");
+});
+
+
+
+
+
+
 
 
 
